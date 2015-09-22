973fb4da58c615cb3ababe1f48ef65b6534cce9a
Yun Liu <liuyun.97@gmail.com>
2015-09-22 17:58:10 +0800

    Better logging

--- culogin.sh
+++ culogin.sh
@@ -11,24 +11,25 @@ UA="Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/600.1.4 (KHTML, lik
 
 login() {
 RESPONSE=`curl "http://114.247.41.52:808/protalAction!portalAuth.action?" -H "Cookie: $COOKIE" -H "Origin: http://114.247.41.52:808" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: zh-CN,zh;q=0.8" -H "User-Agent: $UA" -H "Content-Type: application/x-www-form-urlencoded" -H "Accept: application/json, text/javascript, */*" -H "Referer: http://114.247.41.52:808/protalAction!index.action?wlanuserip=$WLANUSERIP&basip=61.148.2.182" -H "X-Requested-With: XMLHttpRequest" -H "Connection: keep-alive" --data "wlanuserip=$WLANUSERIP&localIp=&basip=61.148.2.182&lpsUserName=$USERNAME&lpsPwd=$PASSWORD" -s`
-echo $RESPONSE >> $LOGFILE
+echo `date +'%Y-%m-%d %H:%M:%S'` $RESPONSE >> $LOGFILE
 STATUS=`echo $RESPONSE | grep "parsererror"`
 }
 
 start() {
-echo `date +'%Y-%m-%d %H:%M:%S'` >> $LOGFILE
+echo `date +'%Y-%m-%d %H:%M:%S'` "Starting...">> $LOGFILE
 WLANUSERIP=`ifconfig | grep -oE "172\.16\.[0-9]{1,3}\.[0-9]{1,3}" | awk 'NR==1'`
 until [ $WLANUSERIP ]
 do
 	sleep 3
-	echo "Retry for WAN IP" >> $LOGFILE
+	echo `date +'%Y-%m-%d %H:%M:%S'` "Retry for WAN IP." >> $LOGFILE
 	WLANUSERIP=`ifconfig | grep -oE "172\.16\.[0-9]{1,3}\.[0-9]{1,3}" | awk 'NR==1'`
 done
-echo $WLANUSERIP >> $LOGFILE
+echo `date +'%Y-%m-%d %H:%M:%S'` "User IP:" $WLANUSERIP >> $LOGFILE
 COOKIE=`curl 114.247.41.52:808 -I -s --retry 100 | grep "Set-Cookie" | cut -c13-55`
 login
 while [ $STATUS ]
 do 
 	login
 done
+echo `date +'%Y-%m-%d %H:%M:%S'` "Done." >> $LOGFILE
 }
